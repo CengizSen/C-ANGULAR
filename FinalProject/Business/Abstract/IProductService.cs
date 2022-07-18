41ad6c2c3a51d417d2c7cc65eae40d42179352b0
Cengiz Şen <58827136+CengizSen@users.noreply.github.com>
2022-07-18 12:00:54 +0300

    C# ile entity framework
    Entity Framework ile Northwind Sql tabanlı bağlantının yapılması,kurumsal mimari ile operasyonlarının yönetilmesi.Solid ve soap prensiplerine uygun OOP standartlarında kodlama.

---
+++ FinalProject/Business/Abstract/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public interface IProductService
+    {
+        List<Product> GetAll();//Bütün datayı getirir
+        List<Product> GetAllByCategoryId(int id);//Category datasını verilen category Idsine göre getirir   
+        List<Product> GetByUnitPrice(decimal min, decimal max);//Belirtilen fiyat aralığındaki verileri getirir.
+     }
+}
---
+++ FinalProject/DataAccess/Abstract/IEntitiyRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:referans tip
+   public interface IEntitiyRepository<T> where T:class,IEntity,new()
+    {
+
+
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);//Filter null olabilir eğer nullsa tüm veriyi getirir.
+        T Get(Expression<Func<T, bool>> filter);//Category Id gibi değerleri getirmeyi sağlar filter girmen gerekir.
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+
+        
+    }
+}
